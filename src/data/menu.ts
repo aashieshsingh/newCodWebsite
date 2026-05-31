8f259ac5ffd4afb724a7577c657725a32d998357
Ashish <ashishjn94@gmail.com>
2026-05-31 12:53:43 +0530

    changed the urls

--- src/data/menu.ts
+++ src/data/menu.ts
@@ -4,7 +4,7 @@ export const headerMenu = [
     { name: 'Home', link: '/' },
     { name: 'About', link: '/about' },
     { name: 'Team', link: '/team' },
-    { name: 'Register', link: 'https://causeofdeathindia.com/join-us-as-va-coder/', target: '_blank'},
+    { name: 'Register', link: 'https://training.causeofdeathindia.com/join-us-as-va-coder/', target: '_blank'},
     { name: 'Process', link: '/process' },
     { name: 'Gallery', link: '/gallery' },
     { name: 'Data & Insights', link: '/insights' },
@@ -16,7 +16,7 @@ export const headerMenu = [
     
         ]
     },
-    { name: 'Login', link: 'https://causeofdeathindia.com/login/', target: '_blank'},
+    { name: 'Login', link: 'https://training.causeofdeathindia.com/login/', target: '_blank'},
 ];
 
 export const footerMenu = [
